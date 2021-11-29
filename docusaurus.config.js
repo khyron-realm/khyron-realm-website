6c857386f02eed587e23e7a8364e0647ee21534d
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-11-29 19:50:14 +0200

    Updated site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'description',
             position: 'left',
             label: 'Wiki',
           },
