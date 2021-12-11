0e87ce9de1196ebdfc76d00db78d129b5e3256b7
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-12-11 20:26:14 +0200

    Updated hero section

--- src/theme/Hero/index.js
+++ src/theme/Hero/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Image from "@theme/IdealImage";
 import useThemeContext from '@theme/hooks/useThemeContext';
-import mineLight from "@site/static/img/pages/light/mine.png";
-import mineDark from "@site/static/img/pages/dark/mine.png";
 import styles from "./styles.module.scss";
 
 function Hero() {
@@ -14,15 +11,11 @@ function Hero() {
   return (
     <header id="hero" className={isDarkTheme ? clsx("hero", styles.banner_dark) : clsx("hero", styles.banner)}>
       <div className="container">
-
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className={clsx("hero__subtitle", styles.subtitle)}>
           {siteConfig.tagline}
         </p>
-
       </div>
-
-      <Image img={isDarkTheme ? mineDark : mineLight} className={clsx("shadow-md", styles.image)} />
     </header>
   );
 }
