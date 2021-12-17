eb35370581dff5aea06e2dbc4f168d8d0c3fb611
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-12-17 11:09:47 +0200

    Updated build

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,4 +1,5 @@
 export default [
+  require("/Users/alex_grigoras/Documents/GitHub/game-website/node_modules/@docusaurus/plugin-google-gtag/lib/gtag"),
   require("/Users/alex_grigoras/Documents/GitHub/game-website/node_modules/infima/dist/css/default/default.css"),
   require("/Users/alex_grigoras/Documents/GitHub/game-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/Users/alex_grigoras/Documents/GitHub/game-website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
